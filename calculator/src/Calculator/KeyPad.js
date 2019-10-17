34573ffaacb938a849ee6c07ba30f7d0f2679fa4
miloujb <emilyjbailey@hotmail.co.uk>
2019-10-17 19:08:54 +0100

    background design

---
+++ calculator/src/Calculator/KeyPad.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class KeyPad extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="header">header header header header</div>
+          <div className="row1">main main main main</div>
+          <div className="row2">main main main main</div>
+          <div className="row3">main main main main</div>
+          <div className="row4">main main main main</div>
+          <div className="footer">footer footer footer footer</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default KeyPad;
