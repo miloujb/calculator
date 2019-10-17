64039bfa42b5fbec6b2fe4dbdabec6937ea461ba
miloujb <emilyjbailey@hotmail.co.uk>
2019-10-17 19:51:49 +0100

    result screen added

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-
+import Calculator from "./Calculator/Calculator";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Calculator />
     </div>
   );
 }
