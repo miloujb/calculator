d995483a4d9263f077654ee3e8ff4ddd61e8deae
miloujb <emilyjbailey@hotmail.co.uk>
2019-10-18 01:30:55 +0100

    editing to css

--- calculator/src/Calculator/Components/FormulaScreen/FormulaScreen.js
+++ calculator/src/Calculator/Components/FormulaScreen/FormulaScreen.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const FormulaScreen = props => {
   console.log(props);
-  return <div className="formula-screen">{props.children}</div>;
+  return (
+    <div className="formula-screen">
+      <div className="formula">{props.children}</div>
+    </div>
+  );
 };
 
 export default FormulaScreen;
